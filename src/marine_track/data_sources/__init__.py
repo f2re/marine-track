12a4291163bc5d49095c2a106df429daa54ce20c
F2re <f2re@users.noreply.github.com>
2026-07-06 07:42:06 +0300

    Add data sources exports

---
+++ src/marine_track/data_sources/__init__.py
@@ -0,0 +1,13 @@
+from marine_track.data_sources.asf_provider import ASFProvider
+from marine_track.data_sources.base import ProviderError, SceneProvider, SearchRequest, SourceManager
+from marine_track.data_sources.stac_provider import STACProvider, default_stac_providers
+
+__all__ = [
+    "ASFProvider",
+    "ProviderError",
+    "SceneProvider",
+    "SearchRequest",
+    "SourceManager",
+    "STACProvider",
+    "default_stac_providers",
+]
